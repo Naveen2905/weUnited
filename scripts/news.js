4b63cbb7205cf7d65d3abede9c2d7f99baeb9713
Naveen2905 <naveenmalhotra05@gmail.com>
2020-05-18 12:45:35 -0400

    adding files to repo

---
+++ scripts/news.js
@@ -0,0 +1,92 @@
+const worldNewsApp = {
+    query: `"coronavirus" OR "covid-19"`,
+    key: 'dbd2da25f5f84f779e6ee7f7c49c5f7d',
+    pageSize: 20,
+}
+
+
+const newsApp = {
+    key: 'dbd2da25f5f84f779e6ee7f7c49c5f7d',
+    query: 'covid-19',
+    category: 'health',
+    country: 'ca',
+};
+
+//World News---------------------------------
+worldNewsApp.world = function () {
+    $('ul.newsLists').empty(); // empty the ul before fetching and adding new data
+    $.ajax({
+        url: `https://newsapi.org/v2/everything`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apiKey: worldNewsApp.key,
+            q: worldNewsApp.query,
+            pageSize: worldNewsApp.pageSize,
+        }
+    }).then(function (data) {
+        data.articles.forEach(function (eachWorldNews) {
+            const htmlToAppend = `<li>
+                                    <a href=${eachWorldNews.url} target="_blank">
+                                    <h3>${eachWorldNews.title}</h3>
+                                    <img src=${eachWorldNews.urlToImage} alt="${eachWorldNews.title}">
+                                    <p>${eachWorldNews.description}</p>
+                                    </a>
+                                </li>`
+            $('ul.newsLists').append(htmlToAppend);
+        })
+    })
+}
+
+
+//Canada news ----------------------------------
+newsApp.canada = function () {
+    $('ul.newsLists').empty(); // empty the ul before fetching and adding new data
+    $.ajax({
+        url: `https://newsapi.org/v2/top-headlines`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apiKey: newsApp.key,
+            q: newsApp.query,
+            category: newsApp.category,
+            country: newsApp.country,
+        }
+    }).then(function (result) {
+        // Iterrating through array
+        result.articles.forEach(function (eachNews) {
+            const htmlToAppend = `<li>
+                                    <a href=${eachNews.url} target="_blank">
+                                    <h3>${eachNews.title}</h3>
+                                    <img src=${eachNews.urlToImage} alt="${eachNews.title}">
+                                    <p>${eachNews.description}</p>
+                                    </a>
+                                </li>`
+            $('ul.newsLists').append(htmlToAppend);
+        })
+
+    })
+}
+
+
+newsApp.init = function () {
+    worldNewsApp.world();
+    // Function to get country and display news;
+    $('.search a').on('click',function (e) {
+
+        if (e.target.name === 'ca') {
+            newsApp.canada();
+        } else {
+            worldNewsApp.world();
+        }
+        
+        $('.search a').removeClass('selected')
+        $(this).addClass('selected')
+    });
+
+}
+
+
+$(function () {
+    newsApp.init();
+});
